113d7f50d62445abdf5840572778319771d174fb
kssrr <lennart.kasserra@gmail.com>
2025-08-15 18:41:41 +0200

    funktioniert lokal, aber installation scheißt bei S3-Methoden rein

---
+++ R/hashmap.R
@@ -0,0 +1,27 @@
+#' @export
+print.Rcpp_rstruct_hashmap <- function(obj) {
+  obj$print()
+}
+
+#' @export
+`[[.Rcpp_rstruct_hashmap` <- function(obj, key) {
+  obj$get(key)
+}
+
+#' @export
+`[.Rcpp_rstruct_hashmap` <- function(obj, keys) {
+  obj$retrieve(keys)
+}
+
+#' @export
+hashmap <- function(input = NULL) {
+  if (is.null(input)) {
+      return(new(rstruct_hashmap))
+  } 
+
+  if (!is.list(input) || any(is.na(names(input))) || any(names(input) == "")) {
+    stop("Input needs to be a named list, or missing.")
+  }
+  
+  new(rstruct_hashmap, input)
+}
---
+++ src/hashmap.cpp
@@ -0,0 +1,112 @@
+#include <Rcpp.h>
+#include <unordered_map>
+#include <string>
+
+// Hash Map w/ std::string keys storing arbitrary R objects as values
+
+class HashMap {
+    std::unordered_map<std::string, Rcpp::RObject> data_;
+  
+  public:
+    HashMap() {}
+
+    HashMap(const Rcpp::List &input) {
+        Rcpp::CharacterVector names = input.names();
+        if (names.size() != input.size()) {
+            Rcpp::stop("All elements must be named");
+        }
+        data_.reserve(input.size());
+        for (size_t i = 0; i < input.size(); ++i) {
+            data_[Rcpp::as<std::string>(names[i])] = input[i];
+        }
+    }
+
+    bool contains(const std::string &q) const { return data_.find(q) != data_.end(); }
+    
+    void insert(const std::string &k, Rcpp::RObject v) { data_[k] = v; }
+
+    Rcpp::RObject get(const std::string &q) const {
+        auto it = data_.find(q);
+        if (it == data_.end()) {
+            return R_NilValue;
+        }
+        return it->second;
+    }
+
+    void print() const {
+        Rcpp::Rcout << "*Rcpp Hash Map*\n\n";
+        if (data_.empty()) {
+            Rcpp::Rcout << "<empty>\n";
+            return;
+        }
+
+        Rcpp::CharacterVector keys(data_.size());
+        Rcpp::List values(data_.size());
+        
+        size_t i = 0;
+        for (const auto& kv : data_) {
+            keys[i] = kv.first;
+            values[i] = kv.second;
+            ++i;
+        }
+        
+        values.attr("names") = keys;
+
+        // dispatch to R print
+        Rcpp::Function print("print");
+        print(values);
+    }
+
+    // bulk operations
+
+    // bulk insertion
+    void update(const Rcpp::List &input) {
+        Rcpp::CharacterVector names = input.names();
+        size_t n = input.size();
+        data_.reserve(data_.size() + n);
+
+        for (size_t i = 0; i < n; i++) {
+            data_[Rcpp::as<std::string>(names[i])] = input[i];
+        }
+    }
+
+    // bulk lookup
+    Rcpp::LogicalVector lookup(const Rcpp::CharacterVector &Q) const {
+        size_t n = Q.size();
+        Rcpp::LogicalVector res(n);
+
+        for (size_t i = 0; i < n; i++) {
+            res[i] = data_.find(Rcpp::as<std::string>(Q[i])) != data_.end();
+        }
+
+        res.attr("names") = Q; // return named vector
+        return res;
+    }
+
+    // bulk retrieval
+    Rcpp::List retrieve(const Rcpp::CharacterVector &Q) const {
+        size_t n = Q.size();
+        Rcpp::List res(n);
+
+        for (size_t i = 0; i < n; i++) {
+            res[i] = get(Rcpp::as<std::string>(Q[i]));
+        }
+
+        res.attr("names") = Q; // return named list
+        return res;
+    }
+};
+
+RCPP_MODULE(hashmap_module) {
+    Rcpp::class_<HashMap>("rstruct_hashmap")
+        .constructor()
+        .constructor<Rcpp::List>()
+        .method("contains", &HashMap::contains)
+        .method("insert", &HashMap::insert)
+        .method("get", &HashMap::get)
+        .method("update", &HashMap::update)
+        .method("lookup", &HashMap::lookup)
+        .method("retrieve", &HashMap::retrieve)
+        .method("print", &HashMap::print)
+    ;
+}
