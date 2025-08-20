01b5673c76fa461e71bc8dbd6541b51275fb6725
kssrr <lennart.kasserra@gmail.com>
2025-08-20 17:42:18 +0200

    templated

--- src/hashset.cpp
+++ src/hashset.cpp
@@ -1,100 +1,54 @@
-// [[Rcpp::depends(RApiSerialize)]]
-
-/*
- * To store arbitrary R objects in a hash set, we serialize them & then
- * compute a hash digest (larger R objects can become a rather large byte
- * sequence, so a digest is easier to store & hash for `std::unordered_set`).
- */
-
 #include <Rcpp.h>
-#include <unordered_set>
-#include <RApiSerializeAPI.h>
 
 #include "hashset.h"
-#include "MurmurHash3.h"
 
-inline Rcpp::RawVector serializeRcpp(const Rcpp::RObject& obj) {
-  SEXP res = R::serializeToRaw(obj, R_NilValue, R_NilValue);
-  return Rcpp::RawVector(res);
+using HashNum = HashSetNum;
+
+RCPP_MODULE(HashSetNumModule) {
+    using namespace Rcpp;
+
+    class_<HashNum>("HashSetNum")
+    .constructor()
+    .constructor<NumericVector>()
+    .method("contains", &HashNum::contains)
+    .method("insert", &HashNum::insert)
+    .method("remove", &HashNum::remove)
+    .method("lookup", &HashNum::lookup)
+    .method("update", &HashNum::update)
+    .method("print", &HashNum::print)
+    ;
 }
 
-uint32_t digestRObject(const Rcpp::RObject &obj) {
-    Rcpp::RawVector bytes = serializeRcpp(obj);
-    uint32_t out;
-    MurmurHash3_x86_32(RAW(bytes), bytes.size(), 0, &out);
-    return out;
+using HashChr = HashSetChr;
+
+RCPP_MODULE(HashSetChrModule) {
+    using namespace Rcpp;
+
+    class_<HashChr>("HashSetChr")
+    .constructor()
+    .constructor<CharacterVector>()
+    .method("contains", &HashChr::contains)
+    .method("insert", &HashChr::insert)
+    .method("remove", &HashChr::remove)
+    .method("lookup", &HashChr::lookup)
+    .method("update", &HashChr::update)
+    .method("print", &HashChr::print)
+    ;
 }
 
-// Constructors
+using HashGen = HashSetGeneric;
 
-HashSet::HashSet() {}
+RCPP_MODULE(HashSetGenericModule) {
+    using namespace Rcpp;
 
-HashSet::HashSet(const Rcpp::List &input) {
-    R_xlen_t n = input.size();
-    data_.reserve(n);
-
-    for (R_xlen_t i = 0; i < n; i++) {
-        data_.insert(digestRObject(Rcpp::RObject(input[i])));
-    }
-}
-
-// scalar operations
-
-bool HashSet::contains(const Rcpp::RObject &q) const {
-    return data_.find(digestRObject(q)) != data_.end();
-}
-
-void HashSet::insert(const Rcpp::RObject v) { data_.insert(digestRObject(v)); }
-
-void HashSet::remove(const Rcpp::RObject &v) { data_.erase(digestRObject(v)); }
-
-// bulk ops
-
-Rcpp::LogicalVector HashSet::lookup(const Rcpp::List &Q) const {
-    R_xlen_t n = Q.size();
-    Rcpp::LogicalVector res(n);
-
-    for (R_xlen_t i = 0; i < n; i++) {
-        res[i] = data_.find(digestRObject(Q[i])) != data_.end();
-    }
-
-    // not returning a named vector here since the entries
-    // can be arbitrary R-objects (cannot simply be coerced)
-    // to character/names
-    return res;
-}
-
-void HashSet::update(const Rcpp::List &V) {
-    for (const auto &v : V) {
-        data_.insert(digestRObject(Rcpp::RObject(v)));
-    }
-}
-
-void HashSet::print() const {
-    Rcpp::Rcout << "*Rcpp Hash Set*\n";
-
-    if (data_.empty()) {
-        Rcpp::Rcout <<"<empty>\n";
-        return;
-    }
-
-    for (const auto &elem : data_) {
-        Rcpp::Function print("print");
-        print(elem);
-        Rcpp::Rcout << "\n";
-    }
-}
-
-// Expose as Rcpp Module:
-
-RCPP_MODULE(hashset_module) {
-    Rcpp::class_<HashSet>("rstruct_hashset")
-        .constructor()
-        .constructor<Rcpp::List>()
-        .method("contains", &HashSet::contains)
-        .method("insert", &HashSet::insert)
-        .method("update", &HashSet::update)
-        .method("lookup", &HashSet::lookup)
-        .method("print", &HashSet::print)
+    class_<HashGen>("HashSetGeneric")
+    .constructor()
+    .constructor<List>()
+    .method("contains", &HashGen::contains)
+    .method("insert", &HashGen::insert)
+    .method("remove", &HashGen::remove)
+    .method("lookup", &HashGen::lookup)
+    .method("update", &HashGen::update)
+    .method("print", &HashGen::print)
     ;
 }
--- src/hashset.h
+++ src/hashset.h
@@ -1,17 +1,133 @@
 #pragma once
 
-#include <Rcpp.h>
-#include <unordered_set>
+// [[Rcpp::depends(RApiSerialize)]]
 
+/*
+ * Shallow wrapper around `std::unordered_set` to build hash sets for use in R. The idea
+ * is to provide a data structure with faster lookup than base R vectors for problems
+ * where you care mostly about set membership, so you are fine with trading the flexibility
+ * of vectors for the speed of a hash set.
+ */
+
+#include <Rcpp.h>
+#include <RApiSerializeAPI.h>
+
+#include <unordered_set>
+#include <string>
+
+#include "MurmurHash3.h"
+
+/*
+ * For generic R objects, we serialize them & then compute a 32 bit hash digest to
+ * store in a `std::unordered_set<uint32_t>` (as serialized objects can get rather large).
+ * Using a 32bit hash digest is faster/easier than direct hashing of potentially very large
+ * binary objects. Collision probability should be negligible for hash sets of a reasonable
+ * size (if you need a hash set of arbitrary R objects you should probably ask yourself wtf
+ * you are doing anyways imo).
+ *
+ */
+
+inline Rcpp::RawVector serializeRcpp(const Rcpp::RObject& obj) {
+  SEXP res = R::serializeToRaw(obj, R_NilValue, R_NilValue);
+  return Rcpp::RawVector(res);
+}
+
+uint32_t digestRObject(const Rcpp::RObject &obj) {
+    Rcpp::RawVector bytes = serializeRcpp(obj);
+    uint32_t out;
+    MurmurHash3_x86_32(RAW(bytes), bytes.size(), 0, &out);
+    return out;
+}
+
+// generic base class template for a hash set
+
+template <typename RType, typename CppType>
 class HashSet {
-    std::unordered_set<uint32_t> data_;
-  public:
-    HashSet();
-    HashSet(const Rcpp::List &input);
-    bool contains(const Rcpp::RObject &q) const;
-    void insert(const Rcpp::RObject v);
-    void remove(const Rcpp::RObject &v);
-    Rcpp::LogicalVector lookup(const Rcpp::List &Q) const;
-    void update(const Rcpp::List &V);
-    void print() const;
+protected:
+    std::unordered_set<CppType> data_;
+
+public:
+    HashSet() = default;
+
+    HashSet(const RType &input) {
+        R_xlen_t n = input.size();
+        data_.reserve(n);
+        for (R_xlen_t i = 0; i < n; i++)
+            data_.insert(Rcpp::as<CppType>(input[i]));
+    }
+
+    bool contains(const CppType &q) const { return data_.find(q) != data_.end(); }
+    void insert(const CppType &v) { data_.insert(v); }
+    void remove(const CppType &v) { data_.erase(v); }
+
+    Rcpp::LogicalVector lookup(const RType &Q) const {
+        R_xlen_t n = Q.size();
+        Rcpp::LogicalVector res(n);
+        for (R_xlen_t i = 0; i < n; i++)
+            res[i] = data_.find(Rcpp::as<CppType>(Q[i])) != data_.end();
+        return res;
+    }
+
+    void update(const RType &V) {
+        for (R_xlen_t i = 0; i < V.size(); i++)
+            data_.insert(Rcpp::as<CppType>(V[i]));
+    }
+
+    void print() const {
+        Rcpp::Rcout << "C++ hash set with " << data_.size() << " elements.\n";
+    }
 };
+
+/*
+ * Derived classes: hash sets for numeric & character vectors, and 
+ * hash set for generic R objects.
+ *
+ */
+
+// numeric vector maps to std::unordered_set<double>
+class HashSetNum : public HashSet<Rcpp::NumericVector, double> {
+public:
+    using HashSet<Rcpp::NumericVector, double>::HashSet;
+};
+
+// character vector maps to std::unordered_set<std::string>
+class HashSetChr : public HashSet<Rcpp::CharacterVector, std::string> {
+public:
+    using HashSet<Rcpp::CharacterVector, std::string>::HashSet;
+};
+
+class HashSetGeneric : public HashSet<Rcpp::List, uint32_t> {
+public:
+    HashSetGeneric() = default;
+
+    HashSetGeneric(const Rcpp::List &input) {
+        data_.reserve(input.size());
+        for (R_xlen_t i = 0; i < input.size(); ++i)
+            data_.insert(digestRObject(input[i]));
+    }
+
+    bool contains(const Rcpp::RObject &q) const {
+        return data_.find(digestRObject(q)) != data_.end();
+    }
+
+    void insert(const Rcpp::RObject &v) {
+        data_.insert(digestRObject(v));
+    }
+
+    void remove(const Rcpp::RObject &v) {
+        data_.erase(digestRObject(v));
+    }
+
+    Rcpp::LogicalVector lookup(const Rcpp::List &Q) const {
+        Rcpp::LogicalVector res(Q.size());
+        for (R_xlen_t i = 0; i < Q.size(); i++)
+            res[i] = data_.find(digestRObject(Q[i])) != data_.end();
+        return res;
+    }
+
+    void update(const Rcpp::List &V) {
+        for (R_xlen_t i = 0; i < V.size(); ++i)
+            data_.insert(digestRObject(V[i]));
+    }
+};
+
