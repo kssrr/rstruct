acb505f62e760b0554b1ad76d3f77c929ecf0c99
kssrr <lennart.kasserra@gmail.com>
2025-08-22 14:48:58 +0200

    auf template umgestellt & spezialisierte hashsets (numeric/char)

--- R/hashset.R
+++ R/hashset.R
@@ -1,17 +1,41 @@
+#' @title Hashset classes and methods
+#' @description Constructors and methods for rstruct hashsets.
+#' @name hashset
+
+#' @exportClass rstruct_hashset
+setClass("rstruct_hashset", contains = "VIRTUAL")
+
 #' @export
-hashset <- function(input = NULL) {
-  if (is.null(input) || is.list(input)) {
-    # default to generic hash set if no input
-    return(new(rstruct_hashset_generic))
+setGeneric("hashset", function(x) standardGeneric("hashset"))
+#' @export
+setMethod("hashset", "numeric", function(x) new(rstruct_hashset_num, x))
+#' @export
+setMethod("hashset", "character", function(x) new(rstruct_hashset_chr, x))
+#' @export
+setMethod("hashset", "list", function(x) new(rstruct_hashset_generic, x))
+#' @export
+setMethod("hashset", "missing", function(x) {
+  message(paste0(
+    "Empty hashsets are initialized to store arbitrary R objects by default.\n",
+    "This might not always be optimal in terms of performance."
+  ))
+  new(rstruct_hashset_generic)
+})
+
+#' @export
+setMethod(
+  "[", 
+  signature(x = "rstruct_hashset", i = "ANY"),
+  function(x, i, ...) {
+    x$lookup(i)
   }
-  
-  if (is.numeric(input)) {
-    return(new(rstruct_hashset_num, input))
+)
+
+#' @export
+setMethod(
+  "[[", 
+  signature(x = "rstruct_hashset", i = "ANY"),
+  function(x, i, ...) {
+    x$contains(i)
   }
-  
-  if (is.character(input)) {
-    return(new(rstruct_hashset_chr, input))
-  }
-  
-  stop("Unsupported input type for hashset()")
-}
+)
